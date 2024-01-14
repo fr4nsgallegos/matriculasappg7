9ee582bf866a5201e19c266bb162c11e5b3ead00
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-14 11:07:54 -0500

    with classes

--- lib/screens/matricula_screen.dart
+++ lib/screens/matricula_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:matriculasapp/models/Person_model.dart';
 
 class MatriculasHome extends StatefulWidget {
   @override
@@ -6,12 +7,10 @@ class MatriculasHome extends StatefulWidget {
 }
 
 class _MatriculasHomeState extends State<MatriculasHome> {
-  List<String> names = ["ana", "paz", "luana"];
-
-  List<Map<String, dynamic>> peopleList = [
-    {"name": "Ana", "adress": "av 1245", "phone": "12345679"},
-    {"name": "Lia", "adress": "av lima", "phone": "9751665"},
-    {"name": "Maria", "adress": "av mlsis", "phone": "88888888"},
+  List<Person> peopleList = [
+    Person(name: "Ana", address: "av 1245", phone: "12345679"),
+    Person(name: "Lia", address: "av lima", phone: "9751665"),
+    Person(name: "Maria", address: "av mlsis", phone: "88888888"),
   ];
 
   @override
@@ -26,12 +25,11 @@ class _MatriculasHomeState extends State<MatriculasHome> {
           ElevatedButton(
             onPressed: () {
               peopleList.add(
-                {
-                  "name": "Pedro",
-                  "adress": "av larco 789",
-                  "phone": "11111111",
-                  "DNI": "1234567"
-                },
+                Person(
+                    name: "Pedro",
+                    address: "av Larc0 789",
+                    phone: "1938771",
+                    dni: "12345678"),
               );
               setState(() {});
             },
@@ -42,18 +40,14 @@ class _MatriculasHomeState extends State<MatriculasHome> {
                   backgroundColor: Colors.cyan,
                   radius: 25,
                   child: Text(
-                    e["name"][0],
+                    e.name[0],
                     style: TextStyle(fontSize: 20, color: Colors.white),
                   ),
                 ),
-                title: Text(e["name"]),
-                subtitle: Text(e["adress"]),
-                trailing: Text(e["phone"]),
+                title: Text(e.name),
+                subtitle: Text(e.address),
+                trailing: Text(e.phone),
               )),
-          // Text("HIJOS"),
-          // ...names.map((e) => Text("hola", style: TextStyle(fontSize: 30),)),
-          // ...names.map((e) => Text(e, style: TextStyle(fontSize: 30 ),)),
-          // ...List.generate(names.length, (index) => Text(names[index])),
         ],
       ),
     );
